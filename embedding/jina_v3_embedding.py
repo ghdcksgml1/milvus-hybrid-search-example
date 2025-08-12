4b9850c4cafa73d80c5a26ad3111053ad1e77d13
ghdcksgml1 <ghdcksgml2@naver.com>
2025-08-13 01:00:47 +0900

    refact: gpu 사용설정

--- embedding/jina_v3_embedding.py
+++ embedding/jina_v3_embedding.py
@@ -1,10 +1,17 @@
+import torch
 from transformers import AutoModel
 
 from embedding.embedding_result import EmbeddingResult
 
 class JinaV3EmbeddingService:
     def __init__(self):
-        self.embedding_model = AutoModel.from_pretrained("jinaai/jina-embeddings-v3", trust_remote_code=True)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.embedding_model = AutoModel.from_pretrained(
+            "jinaai/jina-embeddings-v3",
+            trust_remote_code=True,
+            device_map="auto",  # 자동 GPU 할당
+            torch_dtype=torch.float16  # 메모리 절약 + 속도 향상
+        ).to(device)
 
     def embed_texts(self, texts: list[str], return_dense: bool = True, return_sparse: bool = True) -> EmbeddingResult:
         print(texts)
